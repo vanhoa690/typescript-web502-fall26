252dad1751fa43b95398fdfcd40b9a4d693f2224
vanhoa690 <vanhoa690@gmail.com>
2026-09-14 19:33:56 +0700

    lab 1

---
+++ learn-typescript/src/lab1.ts
@@ -0,0 +1,6 @@
+// Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
+function chuvihcn(chieudai: number, chieurong: number) {
+  return (chieudai + chieurong) * 2;
+}
+
+console.log(chuvihcn(3, 4));
