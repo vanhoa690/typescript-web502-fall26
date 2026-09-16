4068e741fc3ace219c7a7e18246912eceda66200
vanhoa690 <vanhoa690@gmail.com>
2026-09-16 19:15:30 +0700

    xong lab 2

--- learn-typescript/src/script.js
+++ learn-typescript/src/script.js
@@ -1,1 +1,4 @@
 console.log("javascript");
+
+let age = 25;
+age = "ba muoi";
--- learn-typescript/src/typescript.ts
+++ learn-typescript/src/typescript.ts
@@ -1,1 +1,20 @@
 console.log("typescript");
+
+let age: number = 25;
+age = 30;
+
+let myName: string = "hoadv";
+myName = "20";
+
+let dihockhong: boolean = true;
+dihockhong = false;
+
+let myYear = 2005;
+// myYear = "20008"; => error
+
+// object: chua nhieu thuoc tinh
+const student: { name: string; age: number; isActive: boolean } = {
+  name: "hoadv",
+  age: 34,
+  isActive: true,
+};
