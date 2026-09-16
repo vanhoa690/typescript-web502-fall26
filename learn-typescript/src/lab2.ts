56367220e209e81b4c481917af74f4e1f84a5692
vanhoa690 <vanhoa690@gmail.com>
2026-09-16 19:22:45 +0700

    lab 2 bai 1

--- learn-typescript/src/lab2.ts
+++ learn-typescript/src/lab2.ts
@@ -1,1 +1,14 @@
 // const Product
+// enum
+enum ProductRate {
+  low = "thấp",
+  high = "cao",
+}
+
+const product: { name: string; rate: ProductRate; price: number } = {
+  name: "laptop",
+  rate: ProductRate.low,
+  price: 10000,
+};
+
+console.log(product);
